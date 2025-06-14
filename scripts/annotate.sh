01408e6c515e12606c288884269c0967f8830d12
rohandas14 <rohan.das@colorado.edu>
2025-06-14 00:52:45 -0600

    Fixing annotation slurm script errors.

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --account=blast-lecs-gpu
+#SBATCH --account=blanca-blast-lecs
 #SBATCH --mail-user=roda9210@colorado.edu
 #SBATCH --mail-type=END,FAIL
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
 #SBATCH --time=7-00:00:00
-#SBATCH --qos=blast-lecs-gpu
-#SBATCH --partition=blast-lecs-gpu
-#SBATCH --gres=gpu:1gpu:h100_7g.80gb
+#SBATCH --qos=blanca-blast-lecs
+#SBATCH --partition=blanca-blast-lecs
+#SBATCH --gres=gpu:h100_7g.80gb
 #SBATCH --mem=50G
 #SBATCH --job-name=verbalize_chains
 #SBATCH --output=logs/data.%j.log
@@ -33,4 +33,4 @@ host_ip=$(hostname -i)
 
 # python3 ./annotation/annotate_chains.py -c "immigration" --host $host_ip --workers 8 --save_interval 50
 
-python3 ./annotation/chain_verbalizer.py -c "immigration" --host $host_ip --workers 8 --save_interval 50
+python3 ./annotation/chain_verbalizer.py -c "mfc_immigration" --host $host_ip --workers 8 --domain "immigration" --save_interval 25
