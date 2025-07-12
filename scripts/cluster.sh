58cb8a39bb61ceab51d511d8f835e4960c9cd3bb
rohandas14 <rohan.das@colorado.edu>
2025-07-12 14:43:31 -0600

    Deleting redundant file.

--- scripts/cluster.sh
+++ scripts/cluster.sh
@@ -24,22 +24,14 @@ export HF_HOME="$SLURM_SCRATCH/cache/HF"
 export PYTHONPATH=/scratch/alpine/roda9210/structured-clustering-for-narratives
 
 # Processing annotated chains
-# python3 ./annotation/process_annotated_chains.py -c "immigration"
+# python3 ./clustering/process_event_chains.py -c "mfc_immigration"
 
 # Clustering chains
 
 python3 ./clustering/weighted_pckmeans.py -c "mfc_immigration" -k $1 -w $2
 
-# python3 ./clustering/process_event_chains.py -c "mfc_immigration"
-
-# python3 ./clustering/finetuned_pckmeans.py -c "mfc_immigration" -k 250 -w 0.5
-
-# python3 ./clustering/weighted_pckmeans.py -c "mfc_immigration" -k 250 -w 0.01 --centroid_percentile 25 --pairwise_percentile 15 --skip_init
+# python3 ./clustering/finetuned_pckmeans.py -c "mfc_immigration" -k $1 -w $2
 
 # python3 ./clustering/weighted_pckmeans.py -c "mfc_immigration" -k 250 -w 0.5
 
-# python3 ./clustering/kmeans.py -c "immigration" -k $1
-
-# Running regression
-
-# python3 ./models/regression.py -c "immigration" -k $1 -w $2 --save_results
+# python3 ./clustering/finetuned_pckmeans.py -c "mfc_immigration" -k 250 -w 0.5
